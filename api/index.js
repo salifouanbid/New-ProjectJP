3af1208908ff43e73e5c9011e91897984e8b8451
salifouanbid <2.47415841e+08+salifouanbid@users.noreply.github.com>
2026-09-24 23:56:17 +0000

    Revert "restore Express API entrypoint on Vercel"
    This reverts commit b9f47bb95fbfd3cc81b8fa44d1aab4df88ac7069.

--- api/index.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-// Point d'entrée Vercel de la version stable Express/SQLite.
-// La migration PostgreSQL possède sa propre branche et n'est pas activée ici.
-module.exports = require('../server');
-
